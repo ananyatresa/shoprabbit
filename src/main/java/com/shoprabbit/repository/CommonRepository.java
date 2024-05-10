2a013ff54dc87f954819ce480f0089a94a0c9ce4
ananyatresa <anutresa13@gmail.com>
2024-05-10 14:34:32 +0530

    add databaseconfig

--- src/main/java/com/shoprabbit/repository/CommonRepository.java
+++ src/main/java/com/shoprabbit/repository/CommonRepository.java
@@ -1,10 +1,15 @@
 package com.shoprabbit.repository;
+import com.shoprabbit.config.DatabaseConfig;
 import com.shoprabbit.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommonRepository extends JpaRepository<Product, Long> {
+
+//    @Autowired
+//    DatabaseConfig databaseConfig;
 
     List<Product> findByBrandAndCategory(String brand, String category);
 
